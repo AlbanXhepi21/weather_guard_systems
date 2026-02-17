f1c09b1a26e308903650de136e752b0d68c1aefb
AlbanXhepi21 <axhepi21@gmail.com>
2026-02-17 10:59:42 +0100

    removed the partners part

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,12 @@ import Hero from '../components/sections/Hero';
 import ServicesGrid from '../components/sections/ServicesGrid';
 import WhyChooseUs from '../components/sections/WhyChooseUs';
 import AboutPreview from '../components/sections/AboutPreview';
-import ManufacturerBanner from '../components/sections/ManufacturerBanner';
 import CTABanner from '../components/sections/CTABanner';
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <ManufacturerBanner />
       <section className="bg-white">
         <Container className="pt-8 md:pt-12">
           <SectionHeading
